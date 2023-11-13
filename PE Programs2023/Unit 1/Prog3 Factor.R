016e9b8577ccae6ec5f85328c6781370f5716869
Routhu Siddhartha <92938829+siddhuR@users.noreply.github.com>
2023-11-13 16:33:23 +0530

    Delete PE Programs2023 directory

--- PE Programs2023/Unit 1/Prog3 Factor.R
+++
@@ -1,7 +0,0 @@
-data <- c("East","West","East","North","North","East","West","West","West","East","North")
-print(data)
-factor_data <- factor(data)
-print(factor_data)
-
-blood <- factor(c("O", "AB", "A"),levels = c("A", "B", "AB", "O"))
-blood[1:2]
